f4a7a5b1533505da909100c607108f11f0881f82
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2024-04-28 14:53:02 +0100

    issue#1: MySQL table creation with auto_increment id error

--- backend/src/data_processing/infer_db.py
+++ backend/src/data_processing/infer_db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+import json
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -7,3 +8,21 @@ mydb = mysql.connector.connect(
   password=os.environ["PASSWORD"],
   database="mysql"
 )
+
+cursor = mydb.cursor()
+
+# Define the array
+my_array = ['apple', 'banana', 'cherry']
+
+# Create the table with three columns
+cursor.execute("CREATE TABLE IF NOT EXISTS my_table (id INT AUTO_INCREMENT PRIMARY KEY, f1 VARCHAR(256), f2 VARCHAR(256), f3 VARCHAR(256))")
+
+# Insert the array into the database
+cursor.execute("INSERT INTO my_table (f1, f2, f3) VALUES (%s, %s, %s)", my_array)
+
+# Commit the changes
+mydb.commit()
+
+# Close the cursor and connection
+cursor.close()
+mydb.close()
--- backend/src/data_processing/web_scraping.py
+++ backend/src/data_processing/web_scraping.py
@@ -1,74 +1,81 @@
 from bs4 import BeautifulSoup
+
+from abc import ABC, abstractmethod
 from requests import get
+from requests.exceptions import RequestException
 from typing import Tuple, List
+
 import os
 
-from requests.exceptions import RequestException
 
+class WebScraper(ABC):
+    @abstractmethod
+    def request_data(self, url: str) -> List:
+        raise NotImplementedError
 
-def request_usernames(usernames_page: str) -> List[str]:
-    try:
-        username_response = get(usernames_page)
-    except:
-        raise RequestException
+    @abstractmethod
+    def get_data(self, scraper, store) -> None:
+        raise NotImplementedError
 
-    soup = BeautifulSoup(username_response.content, features="html.parser")
-    pagination = soup.find("div", class_="pagination")
-    is_next = True if pagination.find("a", class_="next") else False
-    id_page: int = 1
-    usernames: List[str] = []
-    while is_next or id_page == 1:
-        get_usernames(soup, usernames)
-        id_page += 1
-        next_page_postfix: str = f"page/{id_page}"
-        next_page_address = os.path.join(username_page, next_page_postfix)
-        username_response = get(next_page_address)
+class UserScraper(WebScraper):
+    def request_data(self, usernames_page: str) -> List[str]:
+        try:
+            username_response = get(usernames_page)
+        except:
+            raise RequestException
+
         soup = BeautifulSoup(username_response.content, features="html.parser")
         pagination = soup.find("div", class_="pagination")
         is_next = True if pagination.find("a", class_="next") else False
-        break
-    return usernames
+        id_page: int = 1
+        usernames: List[str] = []
+        while is_next or id_page == 1:
+            self.get_data(soup, usernames)
+            id_page += 1
+            next_page_postfix: str = f"page/{id_page}"
+            next_page_address = os.path.join(usernames_page, next_page_postfix)
+            username_response = get(next_page_address)
+            soup = BeautifulSoup(username_response.content, features="html.parser")
+            pagination = soup.find("div", class_="pagination")
+            is_next = True if pagination.find("a", class_="next") else False
+            break
+        return usernames
 
-def get_usernames(soup: BeautifulSoup, usernames: List[str]) -> None:
-    usrs = soup.find_all("div", class_="person-summary")
-    for usr in usrs:
-        usr = usr.find("a", class_="name")["href"].split("/")[1]
-        usernames.append(usr)
-    return
+    def get_data(self, soup: BeautifulSoup, usernames: List[str]) -> None:
+        usrs = soup.find_all("div", class_="person-summary")
+        for usr in usrs:
+            usr = usr.find("a", class_="name")["href"].split("/")[1]
+            usernames.append(usr)
+        return
 
-def request_movie_data(target_page: str) -> List[Tuple[str, float]]:
-    try:
-        html_response = get(target_page)
-    except:
-        raise RequestException
+class RatingScraper(WebScraper):
+    def request_data(self, target_page: str) -> List[Tuple[str, float]]:
+        try:
+            html_response = get(target_page)
+        except:
+            raise RequestException
 
-    soup = BeautifulSoup(html_response.content, features="html.parser")
-    pages_div = soup.find("div", class_="paginate-pages")
-    n_pages = int(pages_div.find_all("li", class_="paginate-page")[-1].text)
-
-    all_movie_data: List[Tuple[str, float]] = []
-    for id_page in range(1, n_pages + 1):
-        next_page: str = os.path.join(target_page, f"page/{id_page}")
-        html_response = get(next_page)
         soup = BeautifulSoup(html_response.content, features="html.parser")
-        get_movie_rating(soup, all_movie_data)
-    return all_movie_data
+        pages_div = soup.find("div", class_="paginate-pages")
+        n_pages = int(pages_div.find_all("li", class_="paginate-page")[-1].text)
 
-def get_movie_rating(soup: BeautifulSoup, all_movie_data: List[Tuple[str, float]]) -> None:
-    poster_containers = soup.find_all("li", class_="poster-container")
-    for poster in poster_containers:
-        movie_title = poster.find("img")["alt"]
-        rating: str = poster.find("span", class_="rating").text
-        num_rating = convert_rating(rating)
-        all_movie_data.append((movie_title, num_rating))
-    return
+        all_movie_data: List[Tuple[str, float]] = []
+        for id_page in range(1, n_pages + 1):
+            next_page: str = os.path.join(target_page, f"page/{id_page}")
+            html_response = get(next_page)
+            soup = BeautifulSoup(html_response.content, features="html.parser")
+            self.get_data(soup, all_movie_data)
+        return all_movie_data
 
-def convert_rating(rating: str) -> float:
-    num_rating = float(len(rating)) if rating[-1] != "½" else len(rating) - 0.5
-    return num_rating * 2
+    def get_data(self, soup: BeautifulSoup, all_movie_data: List[Tuple[str, float]]) -> None:
+        poster_containers = soup.find_all("li", class_="poster-container")
+        for poster in poster_containers:
+            movie_title = poster.find("img")["alt"]
+            rating: str = poster.find("span", class_="rating").text
+            num_rating = self.convert_rating(rating)
+            all_movie_data.append((movie_title, num_rating))
+        return
 
-if __name__ == "__main__":
-    target_page: str = "https://letterboxd.com/mattstouche/films/"
-    username_page: str = "https://letterboxd.com/members/popular/this/week/"
-    #print(request_movie_data(target_page))
-    print(request_usernames(username_page))
+    def convert_rating(self, rating: str) -> float:
+        num_rating = float(len(rating)) if rating[-1] != "½" else len(rating) - 0.5
+        return num_rating * 2
