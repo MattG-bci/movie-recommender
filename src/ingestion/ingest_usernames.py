d02674b5d7af385fcf12f360805731df7df1999a
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-02-04 23:18:31 +0000

    Use Typer for running custom command line commands

--- src/ingestion/ingest_usernames.py
+++ src/ingestion/ingest_usernames.py
@@ -1,9 +1,6 @@
-from src.generation.web_scraping import UserScraper
 from src.settings import USERNAME_PAGE, DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT
-from src.schemas.users import User
 from src.generation.generate import generate_usernames
 import asyncpg
-import asyncio
 
 
 async def ingest_usernames() -> None:
@@ -17,9 +14,3 @@ async def ingest_usernames() -> None:
                 INSERT INTO users (username) VALUES ($1)
                 ON CONFLICT (username) DO NOTHING
             """, username)
-
-
-
-
-if __name__ == "__main__":
-    asyncio.run(ingest_usernames())
---
+++ src/main.py
@@ -0,0 +1,18 @@
+import typer
+from src.ingestion.ingest_usernames import ingest_usernames
+import asyncio
+
+
+app = typer.Typer(no_args_is_help=True)
+
+@app.command()
+def ingest_usrs() -> None:
+    asyncio.run(ingest_usernames())
+
+@app.command()
+def run_all_ingestion() -> None:
+    asyncio.run(ingest_usernames())
+
+
+if __name__ == "__main__":
+    app()
