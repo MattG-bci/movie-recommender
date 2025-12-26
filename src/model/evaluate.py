493abd2a30e6bccef368a0c956a7df8e69007482
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-12-26 14:31:48 +0100

    Improve evaluation code + add mape metric

--- src/model/evaluate.py
+++ src/model/evaluate.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 
 import torch
 from pydantic import BaseModel
@@ -7,24 +8,36 @@ logger = logging.getLogger(__name__)
 
 
 class EvalMetrics(BaseModel):
-    loss: float
     mse: float
+    mape: float
 
 
-def calculate_metrics(metrics: dict[str, list[torch.tensor]]) -> EvalMetrics:
-    assert "loss" in metrics
-    assert "predictions" in metrics
-    assert "targets" in metrics
+def calculate_metrics(
+    predictions: list[torch.Tensor], targets: list[torch.Tensor]
+) -> EvalMetrics:
+    calculated_metrics = defaultdict(float)
+    for metric in EvalMetrics.model_fields.keys():
+        match metric:
+            case "mse":
+                mse = calculate_mse(predictions, targets)
+                calculated_metrics["mse"] = mse
+            case "mape":
+                mape = calculate_mape(predictions, targets)
+                calculated_metrics["mape"] = mape
+            case _:
+                raise KeyError("The requested metric has no implementation yet.")
+    return EvalMetrics(**calculated_metrics)
 
-    mean_loss = sum(metrics["loss"]) / len(metrics["loss"])
-    preds = metrics["predictions"]
-    targets = metrics["targets"]
-    mse = calculate_mse(preds, targets)
-    return EvalMetrics(loss=mean_loss, mse=mse)
 
-
-def calculate_mse(preds: list[torch.tensor], targets: list[torch.tensor]) -> float:
+def calculate_mse(preds: list[torch.Tensor], targets: list[torch.Tensor]) -> float:
     preds_tensor = torch.tensor(preds).view(-1)
     targets_tensor = torch.tensor(targets).view(-1)
     mse = torch.mean((preds_tensor - targets_tensor) ** 2).item()
     return mse
+
+
+def calculate_mape(preds: list[torch.Tensor], targets: list[torch.Tensor]) -> float:
+    preds_tensor = torch.tensor(preds).view(-1)
+    targets_tensor = torch.tensor(targets).view(-1)
+    mape = torch.mean((targets_tensor - preds_tensor).abs() / targets_tensor).item()
+    return mape
